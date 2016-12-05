831e325e95be24d97abab9421f45001a6e770a10
Jack Merrell <jackmerrell@Jacks-MacBook-Pro-4.local>
2016-12-05 20:29:38 +0000

    scroll background image position

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,30 @@
 var more = document.getElementById("more");
-var footer = document.getElementById("footer");
+var top = document.getElementById("top");
 
-more.addEventListener('click', morePressed, false);
+var footer = document.getElementById("footer");
+var landing = document.getElementById("landing");
+
+more.addEventListener('click', morePressed, true);
+top.addEventListener('click', topPressed, true);
 
 
 function morePressed() {
-  footer.scrollIntoView({block: "end", behavior: "smooth"});
+  footer.scrollIntoView(false);
+
+}
+
+function topPressed() {
+  landing.scrollIntoView(false);
+
+}
+
+
+$(window).scroll(function () {
+    setBackgroundPosition();
+})
+$(window).resize(function() {
+    setBackgroundPosition();
+});
+function setBackgroundPosition(){
+    $(".image").css('background-position', "-0"+ -(Math.max(document.body.scrollTop, document.documentElement.scrollTop) / 4) + "px");
 }
