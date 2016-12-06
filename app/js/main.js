ba37e2c8e373e9e948797978f04b2c89f3306c32
Jack Merrell <jackmerrell@Jacks-MacBook-Pro-4.local>
2016-12-06 01:23:10 +0000

    Added media queries, added animations to links and fade in transitions

--- app/js/main.js
+++ app/js/main.js
@@ -1,30 +1,31 @@
-var more = document.getElementById("more");
-var top = document.getElementById("top");
+// var more = document.getElementById("more");
+// var top = document.getElementById("top");
+//
+// var footer = document.getElementById("footer");
+// var landing = document.getElementById("landing");
+//
+// more.addEventListener('click', morePressed, true);
+// top.addEventListener('click', topPressed, true);
+//
+//
+// function morePressed() {
+//   footer.scrollIntoView(false);
+//
+// }
+//
+// function topPressed() {
+//   landing.scrollIntoView(false);
+//
+// }
 
-var footer = document.getElementById("footer");
-var landing = document.getElementById("landing");
 
-more.addEventListener('click', morePressed, true);
-top.addEventListener('click', topPressed, true);
-
-
-function morePressed() {
-  footer.scrollIntoView(false);
-
-}
-
-function topPressed() {
-  landing.scrollIntoView(false);
-
-}
-
-
-$(window).scroll(function () {
-    setBackgroundPosition();
-})
-$(window).resize(function() {
-    setBackgroundPosition();
-});
-function setBackgroundPosition(){
-    $(".image").css('background-position', "-0"+ -(Math.max(document.body.scrollTop, document.documentElement.scrollTop) / 4) + "px");
-}
+// $(window).scroll(function () {
+//     setBackgroundPosition();
+// })
+// $(window).resize(function() {
+//     setBackgroundPosition();
+// });
+//
+// function setBackgroundPosition(){
+//     $(".image").css('background-position', "0"+ -(Math.max(document.body.scrollTop, document.documentElement.scrollTop) / 8) + "px");
+// }
